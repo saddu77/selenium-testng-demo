5eda47363e305564dc4ee6101cffdadd55f9b4da
saddu77 <58520714+saddu77@users.noreply.github.com>
2025-08-26 00:11:36 +0530

    Update RunCucumberTest.java

--- src/test/java/com/example/RunCucumberTest.java
+++ src/test/java/com/example/RunCucumberTest.java
@@ -1,20 +1,33 @@
 package com.example;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+// import org.junit.platform.suite.api.ConfigurationParameter;
+// import org.junit.platform.suite.api.IncludeEngines;
+// import org.junit.platform.suite.api.SelectClasspathResource;
+// import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+// import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+// import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.steps,com.example.support")
-@ConfigurationParameter(
-        key = PLUGIN_PROPERTY_NAME,
-        value = "pretty, html:target/cucumber-report.html, json:target/cucumber.json, junit:target/cucumber.xml"
+// @Suite
+// @IncludeEngines("cucumber")
+// @SelectClasspathResource("features")
+// @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.steps,com.example.support")
+// @ConfigurationParameter(
+//         key = PLUGIN_PROPERTY_NAME,
+//         value = "pretty, html:target/cucumber-report.html, json:target/cucumber.json, junit:target/cucumber.xml"
+// )
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = {"stepDefinitions"},
+    plugin = {
+        "pretty",
+        "json:target/cucumber.json",
+        "html:target/cucumber-html-report"
+    },
+    monochrome = true
 )
 public class RunCucumberTest {
     // No code needed. JUnit 5 discovers Cucumber engine + runs features.
